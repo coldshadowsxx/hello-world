74ed9618589990b01db44c74253c74822861634e
coldshadowsxx <56900099+coldshadowsxx@users.noreply.github.com>
2019-10-23 11:59:10 +0100

    finish README
    and mention  insta.py

---
+++ example.py
@@ -0,0 +1,6 @@
+# hello-world
+just another repostiory 
+HI HUWANS!
+
+hubot here, i like node.js and coffeescript (that's what i'm made of!)
+i've had tocos on the insta.py and find then far superior to earth tocos.
